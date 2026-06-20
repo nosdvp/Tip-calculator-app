3fea76fff53b78707a1b57d0368d763fbbb569ae
Andrew Mischenko <mishchenkoa.97@gmail.com>
2026-06-20 13:50:56 +0300

    add react project files

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='mainContainer'>
+        <div className='mainContainer_title'></div>
+        <div className='mainContainer_filling'>
+          <div className='mainContainer_filling_content'>
+            <div className='mainContainer_filling_content_data'></div>
+            <div className='mainContainer_filling_content_calc'></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
